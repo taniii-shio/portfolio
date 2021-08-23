eece13deddb450ad0b0fbc5ecd14aa4cf90ed256
Naoto Shioya <shioya.naoto17@gmail.com>
2021-08-23 11:28:56 +0900

    implement tabs

---
+++ src/components/DisabledTabs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+export default function DisabledTabs() {
+  const [value, setValue] = React.useState(2);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper square>
+      <Tabs
+        value={value}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={handleChange}
+        aria-label="disabled tabs example"
+      >
+        <Tab label="Home" disabled />
+        <Tab label="About" />
+        <Tab label="Portfolio" />
+        <Tab label="Contact" />
+      </Tabs>
+    </Paper>
+  );
+}

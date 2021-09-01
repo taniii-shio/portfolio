c40d0fd70f2f18edeb0db13350e07b522091f414
Naoto Shioya <shioya.naoto17@gmail.com>
2021-09-01 14:30:39 +0900

    responsive about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,12 +10,15 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import Hidden from "@material-ui/core/Hidden";
 
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   table: {
     width: 600,
     margin: "0 auto",
@@ -28,7 +31,22 @@ const useStyles = makeStyles({
   data: {
     fontSize: "20px",
   },
-});
+  mobile: {
+    marginBottom: "30px",
+    maxWidth: "600px",
+  },
+  tableItem: {
+    width: "auto",
+    margin: "0 30px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  tableTitle: {
+    fontWeight: "bold",
+    fontSize: "18px",
+  },
+}));
 
 function createData(name, data) {
   return { name, data };
@@ -43,6 +61,31 @@ const profiles = [
 
 export default function About() {
   const classes = useStyles();
+
+  const mobileTable = (
+    <Container className={classes.mobile}>
+      <div className={classes.tableItem}>
+        <p className={classes.tableTitle}>Name</p>
+        <p>Naoto Shioya</p>
+      </div>
+      <Divider />
+      <div className={classes.tableItem}>
+        <p className={classes.tableTitle}>Date of birth</p>
+        <p>1999/11/17</p>
+      </div>
+      <Divider />
+      <div className={classes.tableItem}>
+        <p className={classes.tableTitle}>Tools</p>
+        <p>Illustrator, Photoshop, Figma</p>
+      </div>
+      <Divider />
+      <div className={classes.tableItem}>
+        <p className={classes.tableTitle}>Language</p>
+        <p>JavaScript, React.js, Next.js</p>
+      </div>
+      <Divider />
+    </Container>
+  );
   return (
     <>
       <Head>
@@ -61,27 +104,30 @@ export default function About() {
           />
         </div>
 
-        <TableContainer>
-          <Table className={classes.table} aria-label="simple table">
-            <TableBody>
-              {profiles.map((profile) => (
-                <TableRow key={profile.name}>
-                  <TableCell
-                    component="th"
-                    scope="profile"
-                    align="center"
-                    className={classes.profile}
-                  >
-                    {profile.name}
-                  </TableCell>
-                  <TableCell align="center" className={classes.data}>
-                    {profile.data}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Hidden smDown>
+          <TableContainer>
+            <Table className={classes.table} aria-label="simple table">
+              <TableBody>
+                {profiles.map((profile) => (
+                  <TableRow key={profile.name}>
+                    <TableCell
+                      component="th"
+                      scope="profile"
+                      align="center"
+                      className={classes.profile}
+                    >
+                      {profile.name}
+                    </TableCell>
+                    <TableCell align="center" className={classes.data}>
+                      {profile.data}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Hidden>
+        <Hidden mdUp>{mobileTable}</Hidden>
         <IconButton href="https://github.com/taniii-shio" target="_blank">
           <GitHubIcon />
         </IconButton>
